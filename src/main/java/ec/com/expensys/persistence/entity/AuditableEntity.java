8b333ab02e3d898db957c94fe4b07e8780fe3b44
adr.es <teban.es@pm.me>
2024-10-12 10:17:32 -0500

    Add access methods to delete field

--- src/main/java/ec/com/expensys/persistence/entity/AuditableEntity.java
+++ src/main/java/ec/com/expensys/persistence/entity/AuditableEntity.java
@@ -2,6 +2,8 @@ package ec.com.expensys.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -18,6 +20,8 @@ public class AuditableEntity {
     @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
 
+    @Getter
+    @Setter
     @Column(name = "is_delete", nullable = false)
     private Boolean isDeleted = Boolean.FALSE;
 }
