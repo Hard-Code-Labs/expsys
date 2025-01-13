7bef4f1afa5c718711c57bcb6c6eee77ee951ed3
adr.es <teban.es@pm.me>
2025-01-13 00:04:37 -0500

    Add flyway table

--- ScriptsDB/3_creates.sql
+++ ScriptsDB/3_creates.sql
@@ -124,3 +124,27 @@ COMMENT ON COLUMN exp.exp_country.ctr_acronym IS 'Based ISO-3166-1 ALPHA-2';
 COMMENT ON COLUMN exp.exp_transaction.trn_month_reference IS 'Month of reference';
 
 COMMENT ON COLUMN exp.exp_person_category.cat_type IS 'Type: I=Income E=Expense';
+
+
+--Para FLYWAY
+create table exp.flyway_schema_history
+(
+    installed_rank integer                 not null
+        constraint flyway_schema_history_pk
+            primary key,
+    version        varchar(50),
+    description    varchar(200)            not null,
+    type           varchar(20)             not null,
+    script         varchar(1000)           not null,
+    checksum       integer,
+    installed_by   varchar(100)            not null,
+    installed_on   timestamp default now() not null,
+    execution_time integer                 not null,
+    success        boolean                 not null
+);
+
+alter table exp.flyway_schema_history
+    owner to exp;
+
+create index flyway_schema_history_s_idx
+    on exp.flyway_schema_history (success);
